bd91d7da83b469f163a63025df8c5305aa2f8642
Aneri Gandhi <40311799+amg10@users.noreply.github.com>
2024-04-13 20:28:11 -0400

    Update media_generation_service.py

--- media_generation_service.py
+++ media_generation_service.py
@@ -21,37 +21,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Example function to generate video from text (replace with your actual implementation)
-def generate_video(text: str):
-    # Your video generation logic here
-    # This is just a placeholder
-    video_file_path = "stop_motion_video.mp4"
-    return video_file_path
-
 def generate_image(txt: str):
     image_path = "someimg.png"
     return image_path
-
-
-@app.post("/generate_video/")
-async def generate_text_to_video(text: str = Form(...)):
-    # Generate video from text
-    video_path = generate_video(text)
-    
-    # Open video file and read it as bytes
-    with open(video_path, "rb") as video_file:
-        video_bytes = video_file.read()
-    
-    return Response(content=video_bytes, media_type="video/mp4")
-
-@app.post("/stream_generate_video/")
-async def stream_video(text: str = Form(...)):
-
-    def stream_video():
-        with open(generate_video(text), "rb") as video_file:
-            yield from video_file
-
-    return StreamingResponse(stream_video(), media_type="video/mp4")
 
 @app.post("/generate_image")
 async def get_image(text: str):
