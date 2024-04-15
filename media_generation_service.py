565532ba2089fba137b00a92a41fa586f3f88495
Pooja Ravi <kuchan2001@gmail.com>
2024-04-15 01:26:58 -0400

    Update media_generation_service.py
    Added code to generate_image() function for obtaining outputs generated by the compressed diffusion model.

--- media_generation_service.py
+++ media_generation_service.py
@@ -7,6 +7,8 @@ import os
 from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 
+from inference import Diffusion
+
 app = FastAPI()
 
 origins = [
@@ -21,8 +23,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def generate_image(txt: str):
-    image_path = "someimg.png"
+def generate_image(txt: str, n: int):
+    
+    #image_path = "someimg.png"
+    
+    classes = {'airplane':0, 'auto':1, 'bird':2, 'cat':3, 'deer':4, 'dog':5, 'frog':6, 'horse':7, 'ship':8, 'truck':9}
+    label = classes[txt]
+    num_imgs = n
+    mpath = '/content/ema_student.ckpt'
+    
+    diffusion = Diffusion()
+
+    img_out,time = diffusion.infer(mpath,label,num_imgs,cfg=0)
+    image_path = '/local/path'
+    
+    cv2.imwrite(image_path,img_out)
+    
+    print("Inference time:",time)
+    
     return image_path
 
 @app.get("/")
